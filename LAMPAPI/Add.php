11ac5562628923a7f16c36e2e464243e0efbde9a
Diego Pitty <diegopitty20@gmail.com>
2025-01-16 14:21:29 -0500

    Update Add.php

--- LAMPAPI/Add.php
+++ LAMPAPI/Add.php
@@ -1,45 +1,3 @@
-<<<<<<< HEAD:LAMPAPI/Add.php
-<?php
-    $inData = getRequestInfo();
-	
-	$phoneNumber = $inData["phoneNumber"];
-	$email = $indata["email"];
-	$firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
-
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $phoneNumber, $email, $firstName, $lastName);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
-
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-?>
-=======
 <?php
     $inData = getRequestInfo();
 
@@ -81,4 +39,3 @@
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
->>>>>>> fb2ba42b4a88327cca8ae1478eb4b3e4e5ec2f0e:api/Add.php
