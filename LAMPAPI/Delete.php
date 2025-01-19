efb44f8463aa7aac5b9d8fdfb0eaf39efd772366
Lucio Ruben Villena <luciovillena7@gmail.com>
2025-01-18 22:10:57 -0500

    Added Update and Delete php files

---
+++ LAMPAPI/Delete.php
@@ -0,0 +1,36 @@
+<?php
+    $inData = getRequestInfo();
+
+    $userId = $inData["userId"];
+    $firstName = $inData["firstName"];
+    
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error) 
+        {
+            returnWithError( $conn->connect_error );
+        } 
+    else
+        {
+            $stmt = $conn->prepare("DELETE from Contacts WHERE UserID = ? AND firsName = ?");
+            $stmt->bind_param("ss", $userId, $firstName);
+            $stmt->execute();
+            $stmt->close();
+            $conn->close();
+            returnWithError("");
+        }
+
+    function getRequestInfo()
+        {
+            return json_decode(file_get_contents('php://input'), true);
+        }
+    function sendResultInfoAsJson( $obj )
+        {
+            header('Content-type: application/json');
+            echo $obj;
+        }
+    function returnWithError( $err )
+        {
+            $retValue = '{"error":"' . $err . '"}';
+            sendResultInfoAsJson( $retValue );
+        }
+?>
