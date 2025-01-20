a423669c7995e1759bb288a2e97625f40cec80a4
Diego Pitty <diegopitty20@gmail.com>
2025-01-20 02:31:52 -0500

    Update Update.php

--- LAMPAPI/Update.php
+++ LAMPAPI/Update.php
@@ -1,9 +1,28 @@
 <?php
     
     $inData = getRequestInfo();
-
-    $userID = $inData["userId"];
     
+    $contactId = $inData["contactId"];
+    $newFirstName = $inData["newFirstName"];
+    $newLastName = $inData["newLastName"];
+    $newPhoneNumber = $inData["newPhoneNumber"];
+    $newEmail = $inData["newEmail"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("Update Contacts SET FirstName = ?, LastName=?, Phone= ?, Email= ? WHERE ID= ?");
+		$stmt->bind_param("ssssi", $newFirstName, $newLastName, $newPhoneNumber, $newEmail, $contactId);
+		$stmt->execute();
+
+		$stmt->close();
+		$conn->close();
+		returnWithInfo( $newFirstName, $newLastName,$newEmail, $newPhoneNumber, $contactId );
+	}
     function getRequestInfo()
     {
         return json_decode(file_get_contents('php://input'), true);
@@ -18,4 +37,10 @@
         $retValue = '{"error":"' . $err . '"}';
         sendResultInfoAsJson( $retValue );
     }
+
+    function returnWithInfo( $newFirstName, $newLastName,$newEmail, $newPhoneNumber, $contactId )
+	{
+		$retValue = '{"id":' . $contactId . ',"newFirstName":"' . $newFirstName . '","newLastName":"' . $newLastName  . ',"newEmail":"' . $newEmail . '","newPhoneNumber":"' . $newPhoneNumber . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
 ?>
