e6e4ffec17c70c417997a08c4434d1ba76a599ca
Mohammed Mamdouh <mmamdouh4370@gmail.com>
2025-01-23 22:48:11 -0500

    Themed up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#e5cdae',
+        'alttext': '#f4efe9',
+        'background': '#5b320e',
+        'darkalt': '#8d5320',
+        'primary': '#f2c582',
+        'secondary': '#b07545',
+        'accent': '#f2d3a1',
+       },
+    },
   },
+  
   plugins: [],
 }
 
